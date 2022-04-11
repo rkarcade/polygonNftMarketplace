c2cfa43eab9b222610f5f82f6e35a63a4f5add23
rkarcade <84317900+rkarcade@users.noreply.github.com>
2022-04-11 18:34:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-export const nftaddress ="0x8a29FB22E790901E448F48d871817d063a701396"
-export const nftmarketaddress = "0x0114278C34103Ec82C394884162d639227B67669"
+// export const nftaddress ="0x8a29FB22E790901E448F48d871817d063a701396"
+// export const nftmarketaddress = "0x0114278C34103Ec82C394884162d639227B67669"
 
 
-// export const nftaddress ="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-// export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const nftaddress ="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
